8c41ff0315c4b06e562791c920cce2fb4bd8c86a
Bianca <164269876+biancaalvess@users.noreply.github.com>
2025-03-29 15:33:21 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,21 +7,13 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
 }
+
+module.exports = nextConfig
 
 mergeConfig(nextConfig, userConfig)
 
